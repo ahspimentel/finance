e247e0b48cd09e83834d4637f428e7240250647e
angelo.henrique <angelo.henrique@cocobambu.com>
2022-10-23 21:26:09 -0300

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CentralComponent } from './components/central/central.component';
+import { ExtratoComponent } from './components/extrato/extrato.component';
+import { FormComponent } from './components/form/form.component';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CentralComponent,
+    ExtratoComponent,
+    FormComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
